8daa2d0a51d3c015e25681d559de40064c7f9947
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-03-26 15:40:41 +0000

    our code

--- models/base_model.py
+++ models/base_model.py
@@ -16,30 +16,22 @@ class BaseModel:
     updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
-        """Instantiation of base model class
-        Args:
-            args: it won't be used
-            kwargs: arguments for the constructor of the BaseModel
-        Attributes:
-            id: unique id generated
-            created_at: creation date
-            updated_at: updated date
-        """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
-                    setattr(self, key, value)
-                if 'id' not in kwargs:
-                    self.id = str(uuid.uuid4())
-                if 'created_at' not in kwargs:
-                    self.created_at = datetime.now()
-                if 'updated_at' not in kwargs:
-                    self.updated_at = datetime.now()
-        else:
+        """Instatntiates a new model"""
+        if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
+        else:
+            if 'created_at' in kwargs:
+                self.created_at = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                self.created_at = datetime.now()
+            if 'updated_at' in kwargs:
+                self.updated_at = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                self.updated_at = datetime.now()
+            del kwargs['created_at'], kwargs['updated_at'], kwargs.get('__class__', None)
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
 
     def __str__(self):
