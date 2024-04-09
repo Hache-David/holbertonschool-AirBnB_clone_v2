48ca4689cc73a181d4ee67ba1430f54f5304d19a
Hache_David <hache.david@free.fr>
2024-04-09 12:40:54 +0000

    Add exercice 4 with pycodestyle checked

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def hello1(text):
+    text_without_space = text.replace('_', ' ')
+    return "C " + text_without_space
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def hello2(text):
+    text_without_space2 = text.replace('_', ' ')
+    return "Python " + text_without_space2
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
