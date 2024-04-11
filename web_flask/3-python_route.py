50e45182eb2354c53190a7d51b01a6693d048682
Hache_David <hache.david@free.fr>
2024-04-11 09:57:59 +0000

    Fix pycodestyle on exercice 4

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,11 +21,13 @@ def hello1(text):
     text_without_space = text.replace('_', ' ')
     return "C " + text_without_space
 
+
 @app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def hello2(text):
     text_without_space2 = text.replace('_', ' ')
     return "Python " + text_without_space2
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
