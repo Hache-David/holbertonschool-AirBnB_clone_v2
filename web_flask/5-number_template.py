8c2d44e82081d9d6420c4243b490282dc6aba5d2
Hache_David <hache.david@free.fr>
2024-04-11 14:28:58 +0000

    Add exercice 6 with pycodestyle checked and template folder

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def hello1(text):
+    text_without_space = text.replace('_', ' ')
+    return "C " + text_without_space
+
+
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+def hello2(text):
+    text_without_space2 = text.replace('_', ' ')
+    return "Python " + text_without_space2
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def numb(n):
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def template(n):
+    return render_template('5-number.htlm', number=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
